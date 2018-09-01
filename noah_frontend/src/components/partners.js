bdcdeddb2d553f8c751216ec0f3d37f0ee7e6609
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-09-01 17:57:23 +0530

    Updated partners page

--- noah_frontend/src/components/partners.js
+++ noah_frontend/src/components/partners.js
@@ -18,14 +18,21 @@ export default () => (
             <h2 className="mb-4">In Association With</h2>
             <hr /><br /><br />
             <div className="row">
-                <div className="col-md-4">
-                    <a target="_blank" href="http://gtechindia.org/"><img className="img-fluid"  src="img/partners/gtech.svg" /></a>
+                <div className="col-md-6">
+                    <a target="_blank" href="http://gtechindia.org/"><img className="img-fluid" src="img/partners/gtech.svg" /></a>
                 </div>
-                <div className="col-md-4">
-                    <a target="_blank" href="https://www.nasscom.in/"><img className="img-fluid"  src="img/partners/nasscom.svg" /></a>
+                <div className="col-md-6">
+                    <a target="_blank" href="https://www.nasscom.in/"><img className="img-fluid" src="img/partners/nasscom.svg" /></a>
                 </div>
-                <div className="col-md-4">
-                    <a target="_blank" href="http://www.prathidhwani.org/"><img className="img-fluid"  src="img/partners/PrathiDhwani.png" /></a>
+            </div>
+            <div className="clearfix"></div>
+        </div>
+        <div className="container text-center">
+            <h2 className="mb-4">Volunteer Partners</h2>
+            <hr /><br /><br />
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <a target="_blank" href="http://www.prathidhwani.org/"><img className="img-fluid" src="img/partners/PrathiDhwani.png" /></a>
                 </div>
             </div>
             <div className="clearfix"></div>
