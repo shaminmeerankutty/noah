76768296c84a1e93f781731c3bc024fc704ac504
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-09-22 21:44:39 +0530

    Added privacy policy file #14

---
+++ noah_frontend/src/components/privacy.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+class Privacy extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <header className="masthead sub-page">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h2 className="text-uppercase text-white">
+                  <strong>PRIVACY POLICY</strong>
+                </h2>
+                <hr />
+              </div>
+            </div>
+          </div>
+        </header>
+        <section id="privacy">
+          <div className="container">
+            <div className="row">
+              
+            </div>
+          </div>
+        </section>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Privacy;
--- noah_frontend/src/index.js
+++ noah_frontend/src/index.js
@@ -7,6 +7,7 @@ import configureStore from './store';
 import HomeView from './components/home'
 import DonationSteps from './components/donationsteps'
 import TermsConditons from './components/termsconditions'
+import Privacy from './components/privacy';
 
 ReactDOM.render(
   <Provider store={configureStore()}>
@@ -15,6 +16,7 @@ ReactDOM.render(
         <Route exact path="/" component={HomeView} />
         <Route exact path="/how-to-donate" component={DonationSteps} />
         <Route exact path="/terms-and-conditions" component={TermsConditons} />
+        <Route exact path="/privacy-policy" component={Privacy} />
       </div>
     </Router>
   </Provider>,
